728a26120f719fc26922d5281b90510e94f7902b
mustafa137608064 <168101748+mustafa137608064@users.noreply.github.com>
2025-04-26 22:34:40 +0330

    Update build.gradle.kts

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    namespace = "com.v2ray.ang"
+    namespace = "com.v2plus.app"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.v2ray.ang"
+        applicationId = "com.v2plus.app"
         minSdk = 21
         targetSdk = 35
         versionCode = 650
