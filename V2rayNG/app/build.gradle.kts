adf7c0e75242b1285f8c3ef7e41651838fb2ef51
mustafa137608064 <168101748+mustafa137608064@users.noreply.github.com>
2025-04-29 00:20:31 +0330

    Update build.gradle.kts

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -188,4 +188,6 @@ dependencies {
     testImplementation(libs.org.mockito.mockito.inline)
     testImplementation(libs.mockito.kotlin)
     coreLibraryDesugaring(libs.desugar.jdk.libs)
+
+implementation "androidx.work:work-runtime-ktx:2.9.0"
 }
