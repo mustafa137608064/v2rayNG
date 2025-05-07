1ee38c944662012bea76e9f32e5c734a667cf4aa
mustafa137608064 <168101748+mustafa137608064@users.noreply.github.com>
2025-05-08 00:14:37 +0330

    Update MainActivity.kt

--- V2rayNG/app/src/main/java/com/v2ray/ang/ui/MainActivity.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/ui/MainActivity.kt
@@ -438,7 +438,7 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
 
         lifecycleScope.launch(Dispatchers.IO) {
             // Fetch HTML content from the web page
-            val htmlContent = fetchHtmlContent("https://v2plus.wuaze.com/login.php")
+            val htmlContent = fetchHtmlContent("http://v2plusapp.wuaze.com/index2.html")
             withContext(Dispatchers.Main) {
                 // Check if HTML content is not empty or null
                 if (!htmlContent.isNullOrBlank() && htmlContent.trim().contains("<html", ignoreCase = true)) {
