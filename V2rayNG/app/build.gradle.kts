08391ee9fb0fa29e1ab1c2578663fac59668b464
mustafa137608064 <168101748+mustafa137608064@users.noreply.github.com>
2025-05-08 23:41:51 +0330

    Update build.gradle.kts

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -173,4 +173,5 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
     implementation("com.google.firebase:firebase-analytics")
 implementation("com.google.firebase:firebase-messaging")
+implementation("com.github.bumptech.glide:glide:4.16.0")
 }
